0955182df86cf5c0e32af8ddca18130ddaa905f5
Silv3r <silv3r@openmailbox.org>
2016-06-24 00:08:29 +0100

    Fix the bot unable to walk over moving NPC

--- PROProtocol/Npc.cs
+++ PROProtocol/Npc.cs
@@ -8,18 +8,23 @@
         public int PositionY { get; private set; }
         public int LosLength { get; private set; }
 
-        public Npc(int id, string name, int x, int y, int losLength)
+        public bool IsMoving { get { return _path.Length > 0; } }
+
+        private string _path;
+
+        public Npc(int id, string name, int x, int y, int losLength, string path)
         {
             Id = id;
             Name = name;
             PositionX = x;
             PositionY = y;
             LosLength = losLength;
+            _path = path;
         }
 
         public Npc Clone()
         {
-            return new Npc(Id, Name, PositionX, PositionY, LosLength);
+            return new Npc(Id, Name, PositionX, PositionY, LosLength, _path);
         }
     }
 }
