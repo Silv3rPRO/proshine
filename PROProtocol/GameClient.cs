dc1001f8280750e9b9534f71cd55532bc0f8fa3f
Silv3r <silv3r@openmailbox.org>
2016-08-19 18:12:28 +0200

    Update the client version to 0.956

--- PROProtocol/GameClient.cs
+++ PROProtocol/GameClient.cs
@@ -88,7 +88,7 @@ namespace PROProtocol
         public event Action<Shop> ShopOpened;
         public event Action<List<Pokemon>> PCBoxUpdated;
 
-        private const string Version = "0.955";
+        private const string Version = "0.956";
 
         private GameConnection _connection;
         private DateTime _lastMovement;
