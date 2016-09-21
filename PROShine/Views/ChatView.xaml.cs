d84a518f3a48034024870d315b9a62542c6a0e77
penguins <pkb369@gmail.com>
2016-09-21 22:41:30 +0100

    Removed notification window active check
    Should fix the issue when sometimes it doesn't alert/play sound even when PRO window isn't active.

--- PROShine/Views/ChatView.xaml.cs
+++ PROShine/Views/ChatView.xaml.cs
@@ -374,8 +374,6 @@ namespace PROShine
         private void PlayNotification()
         {
             Window window = Window.GetWindow(this);
-            if (!window.IsActive || !IsVisible)
-            {
                 IntPtr handle = new WindowInteropHelper(window).Handle;
                 FlashWindowHelper.Flash(handle);
 
@@ -386,7 +384,6 @@ namespace PROShine
                         player.Play();
                     }
                 }
-            }
         }
     }
 }
