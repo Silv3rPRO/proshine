01616fd29312c15a31f5ed6a4cd1a2cdae6472d7
Silv3r <silv3r@openmailbox.org>
2016-12-17 00:45:56 +0100

    Re-add getOpponentFormFromPC (by @Zonz) No idea why the previous merge discarded it.

--- PROBot/Scripting/LuaScript.cs
+++ PROBot/Scripting/LuaScript.cs
@@ -225,6 +225,7 @@ namespace PROBot.Scripting
             _lua.Globals["getPokemonRegionFromPC"] = new Func<int, int, string>(GetPokemonRegionFromPC);
             _lua.Globals["getPokemonOriginalTrainerFromPC"] = new Func<int, int, string>(GetPokemonOriginalTrainerFromPC);
             _lua.Globals["getPokemonGenderFromPC"] = new Func<int, int, string>(GetPokemonGenderFromPC);
+            _lua.Globals["getPokemonFormFromPC"] = new Func<int, int, int>(GetPokemonFormFromPC);
 
             // Battle conditions
             _lua.Globals["isOpponentShiny"] = new Func<bool>(IsOpponentShiny);
@@ -2142,6 +2143,16 @@ namespace PROBot.Scripting
                 return null;
             }
             return Bot.Game.CurrentPCBox[boxPokemonId - 1].Gender;
+        }
+
+        // API: Form of the pokémon in the current box matching the ID. (0 if no form)
+        private int GetPokemonFormFromPC(int boxId, int boxPokemonId)
+        {
+            if (!IsPCAccessValid("getPokemonFormFromPC", boxId, boxPokemonId))
+            {
+                return -1;
+            }
+            return Bot.Game.CurrentPCBox[boxPokemonId - 1].Form;
         }
 
         // API: Status of the pokemon of the current box matching the ID.
