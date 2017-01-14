e8813a9e1e5d7ec93c19073d6b5c72e54e31566f
Zonz <coldfirexxx@yahoo.com>
2017-01-13 23:44:58 -0600

    Add OptionSlider class

---
+++ PROBot/Modules/OptionSlider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PROBot.Modules
+{
+    public class OptionSlider
+    {
+        public event Action<bool> EnabledStateChanged;
+        public event Action<string> NameChanged, DescriptionChanged;
+
+        private bool _isEnabled = false;
+        private string _name, _description;
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    EnabledStateChanged?.Invoke(value);
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    NameChanged?.Invoke(value);
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    DescriptionChanged?.Invoke(value);
+                }
+            }
+        }
+
+        public OptionSlider (string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+    }
+}
