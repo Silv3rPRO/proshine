7ec3656e2183c59a2e35c9d34cf5d8036c49e2ab
Zonz <coldfirexxx@yahoo.com>
2017-01-18 21:18:49 -0600

    Update OptionSlider.cs

--- PROBot/Modules/OptionSlider.cs
+++ PROBot/Modules/OptionSlider.cs
@@ -1,67 +1,15 @@
-﻿using System;
-
 namespace PROBot.Modules
 {
     public class OptionSlider
     {
-        public event Action<bool, int> EnabledStateChanged;
-        public event Action<string, int> NameChanged, DescriptionChanged;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsEnabled { get; set; }
 
-        private bool _isEnabled = false;
-        private string _name, _description;
-        private int _index;
-        
-        public void Reset()
+        public OptionSlider(string name, string description)
         {
-            _isEnabled = false;
-            _name = "Option " + _index + ": ";
-            _description = "Custom option " + _index + " for use in scripts";
-        }
-
-        public bool IsEnabled
-        {
-            get { return _isEnabled; }
-            set
-            {
-                if (_isEnabled != value)
-                {
-                    _isEnabled = value;
-                    EnabledStateChanged?.Invoke(value, _index);
-                }
-            }
-        }
-
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (_name != value)
-                {
-                    _name = value;
-                    NameChanged?.Invoke(value, _index);
-                }
-            }
-        }
-
-        public string Description
-        {
-            get { return _description; }
-            set
-            {
-                if (_description != value)
-                {
-                    _description = value;
-                    DescriptionChanged?.Invoke(value, _index);
-                }
-            }
-        }
-
-        public OptionSlider (string name, string description, int index)
-        {
-            _name = name;
-            _description = description;
-            _index = index;
+            Name = name;
+            Description = description;
         }
     }
 }
