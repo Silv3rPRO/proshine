f0c35951de99f1c6b628371ba568db6f47f81d7d
Zonz <coldfirexxx@yahoo.com>
2017-01-18 21:11:08 -0600

    Convert Options to a Dictionary

--- PROBot/BotClient.cs
+++ PROBot/BotClient.cs
@@ -31,6 +31,7 @@ namespace PROBot
         public event Action ClientChanged;
         public event Action ConnectionOpened;
         public event Action ConnectionClosed;
+        public event Action<OptionSlider> SliderCreated;
 
         public PokemonEvolver PokemonEvolver { get; private set; }
         public MoveTeacher MoveTeacher { get; private set; }
@@ -52,12 +53,24 @@ namespace PROBot
             AutoReconnector = new AutoReconnector(this);
             MovementResynchronizer = new MovementResynchronizer(this);
             Rand = new Random();
-            Options = new OptionSlider[] { new OptionSlider("Option 1: ", "Custom option 1 for use in scripts", 1),
-                                           new OptionSlider("Option 2: ", "Custom option 2 for use in scripts", 2),
-                                           new OptionSlider("Option 3: ", "Custom option 3 for use in scripts", 3),
-                                           new OptionSlider("Option 4: ", "Custom option 4 for use in scripts", 4),
-                                           new OptionSlider("Option 5: ", "Custom option 5 for use in scripts", 5)
-            };
+            Options = new Dictionary<int, OptionSlider>();
+        }
+
+        public void CreateSlider(int index, bool enable)
+        {
+            Options[index] = new OptionSlider("Option " + index + ": ", "Custom option " + index + " for use in scripts");
+            Options[index].IsEnabled = enable;
+            SliderCreated?.Invoke(Options[index]);
+        }
+
+        public void CreateSlider(int index, string content, bool isName)
+        {
+            if (isName)
+                Options[index] = new OptionSlider(content, "Custom option " + index + " for use in scripts");
+            else
+                Options[index] = new OptionSlider("Option " + index + ": ", content);
+
+            SliderCreated?.Invoke(Options[index]);
         }
 
         public void LogMessage(string message)
