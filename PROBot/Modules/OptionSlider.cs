0075b7c0ec87032b45b6d2bf8f224cbb369c72a0
Zonz <coldfirexxx@yahoo.com>
2017-01-21 18:26:51 -0600

    Revert to action driven system

--- PROBot/Modules/OptionSlider.cs
+++ PROBot/Modules/OptionSlider.cs
@@ -1,15 +1,57 @@
+using System;
+
 namespace PROBot.Modules
 {
     public class OptionSlider
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public bool IsEnabled { get; set; }
+        public event Action StateChanged;
+
+        private bool _isEnabled = false;
+        private string _name, _description;
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    StateChanged?.Invoke();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    StateChanged?.Invoke();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    StateChanged?.Invoke();
+                }
+            }
+        }
 
         public OptionSlider(string name, string description)
         {
-            Name = name;
-            Description = description;
+            _name = name;
+            _description = description;
         }
     }
 }
