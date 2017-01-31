5f665c39a4983db1d5ce7f5564d6134ef23192a9
Zonz <coldfirexxx@yahoo.com>
2017-01-30 19:26:12 -0600

    PropertyChanged Action and Text Options
    Renamed OptionSlider to CustomOptions

---
+++ PROBot/Modules/CustomOptions.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+
+namespace PROBot.Modules
+{
+    public class OptionSlider : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isEnabled = false;
+        private string _name, _description;
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+                }
+            }
+        }
+
+        public OptionSlider(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+    }
+
+    public class TextOption : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name, _description, _content;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+                }
+            }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                if (_content != value)
+                {
+                    _content = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Content"));
+                }
+            }
+        }
+
+        public TextOption(string name, string description, string content)
+        {
+            _name = name;
+            _description = description;
+            _content = content;
+        }
+    }
+}
