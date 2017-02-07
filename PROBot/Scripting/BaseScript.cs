3c4aef9bdd427454204dd84e27e1e9486d863af7
Zonz <coldfirexxx@yahoo.com>
2017-02-06 18:03:17 -0600

    Add Invokes List

--- PROBot/Scripting/BaseScript.cs
+++ PROBot/Scripting/BaseScript.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PROBot.Scripting
 {
@@ -23,6 +24,8 @@ namespace PROBot.Scripting
         public virtual void OnLearningMove(string moveName, int pokemonIndex) { }
 
         public abstract bool ExecuteNextAction();
+
+        public List<Invoker> Invokes = new List<Invoker>();
 
         protected void LogMessage(string message)
         {
