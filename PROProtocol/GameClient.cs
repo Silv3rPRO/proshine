336a5d2646bdc23dd5fcaed2d94877314d84601d
Zonz <coldfirexxx@yahoo.com>
2017-02-27 16:45:49 -0600

    Add IsMember

--- PROProtocol/GameClient.cs
+++ PROProtocol/GameClient.cs
@@ -33,6 +33,7 @@ namespace PROProtocol
 
         public int Money { get; private set; }
         public int Coins { get; private set; }
+        public bool IsMember { get; private set; }
         public List<Pokemon> Team { get; private set; }
         public List<Pokemon> CurrentPCBox { get; private set; }
         public List<InventoryItem> Items { get; private set; }
@@ -1207,6 +1208,7 @@ namespace PROProtocol
             PokedexOwned = Convert.ToInt32(playerData[4]);
             PokedexSeen = Convert.ToInt32(playerData[5]);
             PokedexEvolved = Convert.ToInt32(playerData[6]);
+            IsMember = playerData[10] == "1";
         }
 
         private void OnUpdateTime(string[] data)
