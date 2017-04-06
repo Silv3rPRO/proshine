9c9a0bb79f915ca64fe23996a0e0f7473384e88e
tankyx <tanguy.pedrazzoli@epitech.eu>
2017-04-06 14:39:40 +0200

    update to spearow version

--- PROProtocol/GameClient.cs
+++ PROProtocol/GameClient.cs
@@ -92,7 +92,7 @@ namespace PROProtocol
         public event Action<Shop> ShopOpened;
         public event Action<List<Pokemon>> PCBoxUpdated;
 
-        private const string Version = "0.961";
+        private const string Version = "0.97";
 
         private GameConnection _connection;
         private DateTime _lastMovement;
